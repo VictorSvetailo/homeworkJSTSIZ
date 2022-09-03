1c358b4496f357353304ec9fc1d82312801f9b71
VictorSvetailo <victorioeve@gmail.com>
2022-09-03 20:15:10 +0300

    The homework is over.  Aided design.

--- src/p1-main/m1-ui/u1-app/App.tsx
+++ src/p1-main/m1-ui/u1-app/App.tsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import HW1 from '../../../p2-homeworks/h1/HW1'
 import styles from './App.module.css'
+import HW3 from '../../../p2-homeworks/h3/HW3';
 
 function App() {
     return (
         <div className={styles.app}>
             <div>react homeworks:</div>
-            <HW1/>
+            {/*<HW1/>*/}
             {/*<HW2/>*/}
-            {/*<HW3/>*/}
+            <HW3/>
             {/*<HW4/>*/}
             {/*<HW5/>*/}
 
--- src/p2-homeworks/h3/HW3.tsx
+++ src/p2-homeworks/h3/HW3.tsx
@@ -1,19 +1,29 @@
 import React, {useState} from 'react'
 import GreetingContainer from './GreetingContainer'
+import {v1} from 'uuid';
 
 // types
 export type UserType = {
-    _id: any // need to fix any
-    name: any // need to fix any
+    id: string // need to fix any
+    name: string // need to fix any
 }
 
 // уровень работы с глобальными данными
 function HW3() {
-    const [users, setUsers] = useState<any>([]) // need to fix any
-
-    const addUserCallback = (name: any) => { // need to fix any
-        setUsers([]) // need to fix
+    const [users, setUsers] = useState<Array<UserType>>([])
+    const [userName, setUsersName] = useState<string>('')
+    const addUserCallback = (name: string) => { // need to fix any
+       setUsersName(name)
     }
+
+    const addUserOnLine = (name: string) => {
+        const addUser = {id: v1(), name: name}
+        setUsers([addUser,...users])
+    }
+
+    const allUsers = users.map(u => {
+        return <div key={u.id}>{u.name}</div>
+    })
 
     return (
         <div>
@@ -21,9 +31,10 @@ function HW3() {
             homeworks 3
 
             {/*should work (должно работать)*/}
-            <GreetingContainer users={users} addUserCallback={addUserCallback}/>
+            <GreetingContainer users={users} addUserOnLine={addUserOnLine} userName={userName} addUserCallback={addUserCallback}/>
 
             <hr/>
+            {allUsers}
             {/*для личного творчества, могу проверить*/}
             {/*<AlternativeGreeting/>*/}
             <hr/>
